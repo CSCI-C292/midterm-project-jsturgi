71bcaac40c4c8e093a060b68890ad626d777ddbe
jsturgi <jsturgi@iu.edu>
2020-10-19 14:46:34 -0400

    holy moly guacamole am i overscoped ha ha HA

---
+++ zoom!/Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] Camera cam;
+    float currentTilt = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cam.transform.localEulerAngles = new Vector3(currentTilt, 0, 0);
+        if (Input.GetButtonDown("Launch"))
+        {
+            this.transform.position += new Vector3(1, 1, 0);
+        }
+    }
+}
